4856c6b409e25707c6d11a7e01e546ab279cd902
Vladimir Gusev <bo-ba@mail.ru>
2013-10-24 01:12:56 -0400

    OK, was able to put most of the code back into triangulate, using make_shared

--- source/Triangulator.cpp
+++ source/Triangulator.cpp
@@ -22,71 +22,73 @@ int user_data;
 
 
     
-    //pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+using namespace boost;
 
 void Triangulator::triangulate(std::vector<std::vector<float> >& points){
 
-//    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-//    
-//    cloud->width  = points.size();
-//    cloud->height = 1;
-//    cloud->is_dense = false;
-//    cloud->points.resize (points.size());
-//    
-//    for (size_t i = 0; i < cloud->points.size (); ++i)
-//    {
-//        pcl::PointXYZ& pt = cloud->points[i];
-//        pt.z = points[i][0];
-//        pt.x = points[i][1];
-//        pt.y = points[i][2];
-//    }
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = boost::make_shared<pcl::PointCloud<pcl::PointXYZ> >();
+    
+    cloud->width  = points.size();
+    cloud->height = 1;
+    cloud->is_dense = false;
+    cloud->points.resize (points.size());
+    
+    for (size_t i = 0; i < cloud->points.size (); ++i)
+    {
+        pcl::PointXYZ& pt = cloud->points[i];
+        pt.z = points[i][0];
+        pt.x = points[i][1];
+        pt.y = points[i][2];
+    }
     
     // Normal estimation*
-//    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
-//    pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
-    pcl::search::KdTree<pcl::PointXYZ>::Ptr  bTree = boost::make_shared<pcl::search::KdTree<pcl::PointXYZ> >();
-//    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
-//    tree->setInputCloud (cloud);
-//    n.setInputCloud (cloud);
-//    n.setSearchMethod (tree);
-//    n.setKSearch (20);
-//    n.compute (*normals);
+    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> n;
+    pcl::PointCloud<pcl::Normal>::Ptr normals = boost::make_shared<pcl::PointCloud<pcl::Normal> >();
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr  tree = boost::make_shared<pcl::search::KdTree<pcl::PointXYZ> >();
+
+    tree->setInputCloud (cloud);
+    n.setInputCloud (cloud);
+    n.setSearchMethod (tree);
+    n.setKSearch (20);
+    n.compute (*normals);
     //* normals should not contain the point normals + surface curvatures
     
     // Concatenate the XYZ and normal fields*
-//    pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointNormal>);
-//    pcl::concatenateFields (*cloud, *normals, *cloud_with_normals);
+    pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals = make_shared<pcl::PointCloud<pcl::PointNormal> >();
+    pcl::concatenateFields (*cloud, *normals, *cloud_with_normals);
     //* cloud_with_normals = cloud + normals
     
     // Create search tree*
-//    pcl::search::KdTree<pcl::PointNormal>::Ptr tree2 (new pcl::search::KdTree<pcl::PointNormal>);
-//    tree2->setInputCloud (cloud_with_normals);
-    
+    pcl::search::KdTree<pcl::PointNormal>::Ptr tree2 = make_shared<pcl::search::KdTree<pcl::PointNormal> >();
+    tree2->setInputCloud (cloud_with_normals);
+
     // Initialize objects
-//      pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
-//    pcl::PolygonMesh triangles;
-//    
+    pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
+
+    pcl::PolygonMesh::Ptr triangles = make_shared<pcl::PolygonMesh>();
+
+    //
 //    // Set the maximum distance between connected points (maximum edge length)
-//    gp3.setSearchRadius (0.025);
-//    
-//    // Set typical values for the parameters
-//    gp3.setMu (2.5);
-//    gp3.setMaximumNearestNeighbors (100);
-//    gp3.setMaximumSurfaceAngle(M_PI/4); // 45 degrees
-//    gp3.setMinimumAngle(M_PI/18); // 10 degrees
-//    gp3.setMaximumAngle(2*M_PI/3); // 120 degrees
-//    gp3.setNormalConsistency(false);
-//    
+    gp3.setSearchRadius (0.025);
+        return;    
+    // Set typical values for the parameters
+    gp3.setMu (2.5);
+    gp3.setMaximumNearestNeighbors (100);
+    gp3.setMaximumSurfaceAngle(M_PI/4); // 45 degrees
+    gp3.setMinimumAngle(M_PI/18); // 10 degrees
+    gp3.setMaximumAngle(2*M_PI/3); // 120 degrees
+    gp3.setNormalConsistency(false);
+//
 //    // Get result
-//    gp3.setInputCloud (cloud_with_normals);
-//    gp3.setSearchMethod (tree2);
-//    gp3.reconstruct (triangles);
-//    
-//    std::cout<<triangles<<std::endl;
-//    // Additional vertex information
-//    std::vector<int> parts = gp3.getPartIDs();
-//    std::vector<int> states = gp3.getPointStates();
-//    std::cout<<&parts<<std::endl;
+    gp3.setInputCloud (cloud_with_normals);
+    gp3.setSearchMethod (tree2);
+    gp3.reconstruct (*triangles);
+    
+    std::cout<<*triangles<<std::endl;
+    // Additional vertex information
+    std::vector<int> parts = gp3.getPartIDs();
+    std::vector<int> states = gp3.getPointStates();
+    std::cout<<&parts<<std::endl;
     
     //pcl::visualization::PCLVisualizer& viewer;
     
